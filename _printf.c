0251f86eb08f3e614d63ed491ed2bc9892a92fd6
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-26 16:05:50 +0200

    Create _printf.c

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+* _printf - prints characters
+* format: character string
+* Return: 0 for success, -1 for failure
+*/
+
+int _printf(const char *format, ...)
+{
+
+}
