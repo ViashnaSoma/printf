1d93338323710e76d9643dfd78cfdfd7fcc00087
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-29 11:07:26 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -38,7 +38,7 @@ int _printf(const char *format, ...)
 			switch (format[i + 1])
 			{
 			case 'c':
-				len++;
+				len = len + 1;
 			case 's':
 				/*needs to go to next argument and count number of characters and add to len*/
 				str = va_arg(ap, char *);
